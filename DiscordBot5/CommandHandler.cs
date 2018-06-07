48651ffe46cb9f4c40915834a1ad263b83dec8d5
HoLLy <dario.db@hotmail.com>
2018-06-07 22:48:22 +0200

    Have a basic ncalc command

--- DiscordBot5/CommandHandler.cs
+++ DiscordBot5/CommandHandler.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord.WebSocket;
+using NCalc;
 
 namespace HoLLy.DiscordBot
 {
@@ -22,7 +23,7 @@ namespace HoLLy.DiscordBot
 
             string resp;
             if (content.StartsWith(_prefix)) {
-                string command = content.Substring(_prefix.Length);
+                string command = content.Substring(_prefix.Length).TrimStart();
                 resp = await HandleCommand(msg, command);
             } else {
                 var sb = new StringBuilder();
@@ -37,16 +38,29 @@ namespace HoLLy.DiscordBot
 
         private async Task<string> HandleCommand(SocketMessage msg, string cmd)
         {
+            // For now, have simple command parsing that ignores if the user is trusted.
+            var sidx = cmd.IndexOf(" ");
+            if (sidx == -1) {
+                // No parameters
+            } else {
+                var verb = cmd.Substring(0, sidx);
+                var param = cmd.Substring(sidx + 1);
+                switch (verb.ToLower()) {
+                    case "calc":
+                        return new Expression(param).Evaluate().ToString() ?? "null";
+                }
+            }
+
             return "Pong: " + cmd;
         }
 
         private IEnumerable<KeyValuePair<string, string>> ExtractCommands(string src)
         {
             int unnamedCount = 0;
-            foreach (Match match in Regex.Matches(src, $@"{{{Regex.Escape(_prefix)}(?:(?<name>[^|}}]+)\|)?(?<cmd>[^}}]+)}}"))
+            foreach (Match match in Regex.Matches(src, $@"{{{Regex.Escape(_prefix)}(?:(?<name>[^|}}]+) ?\| ?)?(?<cmd>[^}}]+)}}"))
                 yield return new KeyValuePair<string, string>(
                     match.Groups["name"].Success ? match.Groups["name"].Value : "cmd" + ++unnamedCount,
-                    match.Groups["cmd"].Value);
+                    match.Groups["cmd"].Value.Trim());
         }
     }
 }
