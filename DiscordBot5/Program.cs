002f2018166eb474a9fca6aeddabb3621c67df57
HoLLy <dario.db@hotmail.com>
2018-06-07 19:28:54 +0200

    Add project files.

---
+++ DiscordBot5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBot5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
