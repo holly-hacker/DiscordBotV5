b773fbc2d038f815ee05a7d05d942b60d75feb96
HoLLy <dario.db@hotmail.com>
2018-06-07 22:03:26 +0200

    Implement login and command detection

--- DiscordBot5/Program.cs
+++ DiscordBot5/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
-namespace DiscordBot5
+namespace HoLLy.DiscordBot
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private const string TokenEnvName = "DISCORD_BOT_SECRET";
+        private const string Prefix = "h:";
+
+        private static CommandHandler _cmd;
+
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!\n");
+
+            string token = Environment.GetEnvironmentVariable(TokenEnvName);
+            if (string.IsNullOrWhiteSpace(token)) {
+                Console.WriteLine($"No token in {TokenEnvName}.");
+                Console.ReadLine();
+                return;
+            }
+
+            _cmd = new CommandHandler(Prefix);
+
+            var client = new DiscordSocketClient();
+            Console.WriteLine("Logging in...");
+            await client.LoginAsync(TokenType.Bot, token);
+            Console.WriteLine("Starting...");
+            await client.StartAsync();
+            Console.WriteLine("Logged in.");
+
+            // Setting up events
+            await client.SetGameAsync("dead");
+            client.Log += ClientOnLog;
+            client.MessageReceived += ClientOnMessageReceived;
+
+            await Task.Delay(-1);
+        }
+
+        private static async Task ClientOnMessageReceived(SocketMessage arg)
+        {
+            await _cmd.Handle(arg);
+        }
+
+        private static Task ClientOnLog(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
         }
     }
 }
